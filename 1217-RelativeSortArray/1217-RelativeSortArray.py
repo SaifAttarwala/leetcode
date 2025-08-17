68b77ef7b97d8e123649db76fb1135e9b46acb72
Saif Attarwala <89320752+SaifAttarwala@users.noreply.github.com>
2025-08-18 04:02:26 +0530

    Create 1217-RelativeSortArray

---
+++ 1217-RelativeSortArray/1217-RelativeSortArray.py
@@ -0,0 +1,17 @@
+# Last updated: 8/18/2025, 4:02:17 AM
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        res = []
+
+        for x in range(len(arr2)):
+            for y in range(len(arr1)):
+                if arr1[y] == arr2[x]:
+                    res.append(arr1[y])
+                    arr1[y] = -1
+        arr1.sort()
+
+        for x in arr1:
+            if x != -1:
+                res.append(x)
+
+        return res 
