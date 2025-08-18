493f71c7a314eab00e818f31e24fa2838d216557
Saif Attarwala <89320752+SaifAttarwala@users.noreply.github.com>
2025-08-19 04:03:39 +0530

    Create 832-FlippinganImage

---
+++ 832-FlippinganImage/832-FlippinganImage.py
@@ -0,0 +1,10 @@
+# Last updated: 8/19/2025, 4:03:30 AM
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        
+        image = [x[::-1] for x in image]
+
+        image = [[1 if x == 0 else 0 for x in row] for row in image]
+
+        return image
+        
