683bfaae3c96a587f554fcccad92488a723e59d7
Saif Attarwala <89320752+SaifAttarwala@users.noreply.github.com>
2025-08-20 05:52:01 +0530

    File updated at 8/20/2025, 5:51:50 AM

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.py
@@ -1,7 +1,5 @@
-# Last updated: 8/20/2025, 5:50:08 AM
+# Last updated: 8/20/2025, 5:51:50 AM
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        nums = [x * x for x in nums]
-        nums.sort()
-        return nums
+        return sorted([x * x for x in nums])
         
