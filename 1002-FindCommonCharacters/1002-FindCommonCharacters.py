efd31e46bee9286e4c5e3c5af3f6b1fc201138ea
Saif Attarwala <89320752+SaifAttarwala@users.noreply.github.com>
2025-08-20 23:21:15 +0530

    Create 1002-FindCommonCharacters

---
+++ 1002-FindCommonCharacters/1002-FindCommonCharacters.py
@@ -0,0 +1,10 @@
+# Last updated: 8/20/2025, 11:21:04 PM
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        
+        res = Counter(words[0])
+
+        for x in words:
+            res &= Counter(x)
+        
+        return list(res.elements())
