ede77aa7183ee834a4005c7d27f823f73aaa00cf
Saif Attarwala <89320752+SaifAttarwala@users.noreply.github.com>
2025-08-24 01:22:57 +0530

    Create 997-FindtheTownJudge

---
+++ 997-FindtheTownJudge/997-FindtheTownJudge.py
@@ -0,0 +1,19 @@
+# Last updated: 8/24/2025, 1:22:58 AM
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if n == 1:
+            return 1
+        
+        trust_scores = [0 for x in range(n+1)]
+        print(trust_scores)
+        for person1, person2 in trust:
+            trust_scores[person1] -= 1
+            trust_scores[person2] += 1
+        print(trust_scores)
+        
+        for x in range(1,n+1):
+            if trust_scores[x] == n-1:
+                return x
+        return -1
+
+        
