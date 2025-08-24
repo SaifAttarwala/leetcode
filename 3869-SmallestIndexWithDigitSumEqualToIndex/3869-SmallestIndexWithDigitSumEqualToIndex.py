9ede37c9ebefdee99ee4bb92efa048054cb6e538
Saif Attarwala <89320752+SaifAttarwala@users.noreply.github.com>
2025-08-24 23:54:54 +0530

    Create 3869-SmallestIndexWithDigitSumEqualToIndex

---
+++ 3869-SmallestIndexWithDigitSumEqualToIndex/3869-SmallestIndexWithDigitSumEqualToIndex.py
@@ -0,0 +1,10 @@
+# Last updated: 8/24/2025, 11:54:53 PM
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for x in range(len(nums)):
+            if sum(map(int,str(nums[x]))) == x:
+                return x
+        return -1
+
+        
+        
